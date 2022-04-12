7ce967492de5d73b0c1aa6700abef6b0be7cb3b1
Kunal Kumar Shrivastava <31282058+Kunalkshrivastava@users.noreply.github.com>
2022-04-13 02:14:45 +0530

    Time: 3 ms (55.56%), Space: 44.9 MB (87.15%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,4 +1,24 @@
 class Solution {
+    public int tab(int m,int n,int[][] grid){
+        int[][] tab = new int[m][n];
+        for(int i=m-1; i>=0; i--){
+            for(int j= n-1; j>=0; j--){
+                if(i == m-1 && j == n-1){
+                    tab[i][j] = grid[i][j];
+                    continue;
+                }
+                int ans = (int)(1e8);
+                
+                if(i+1 < m)
+                    ans = Math.min(ans,tab[i+1][j]);
+                if(j+1 < n)
+                    ans = Math.min(ans,tab[i][j+1]);
+                
+                tab[i][j] = ans + grid[i][j];
+            }
+        }
+        return tab[0][0];
+    }
     public int fun(int i,int j, int m,int n,int[][] grid, int[][] mem){
         if(i == m-1 && j == n-1){
             return mem[i][j] = grid[i][j];
@@ -16,6 +36,7 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] mem = new int[m][n];
-        return (fun(0,0,m,n,grid,mem));
+        return tab(m,n,grid);
+        //return (fun(0,0,m,n,grid,mem));
     }
 }
