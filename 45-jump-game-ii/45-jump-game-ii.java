e8e038988909a620b024ada889e08c3dc184330e
Kunal Kumar Shrivastava <31282058+Kunalkshrivastava@users.noreply.github.com>
2022-04-12 23:27:08 +0530

    Time: 28 ms (41.69%), Space: 42.9 MB (83.67%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,4 +1,26 @@
 class Solution {
+    public static int tab(int n,int[] nums){
+        int[] dp = new int[n];
+        
+        for(int i=n-1; i>=0; i--){
+            if(i == n-1){
+                dp[i] = 0;
+                continue;
+            }
+            int ans = (int)(1e8);
+            int jumps = nums[i];
+            for(int j=1; j<= jumps; j++){
+                if(i+j < n){
+                    ans = Math.min(ans,dp[i+j]);
+                }
+                else{
+                    break;
+                }
+            }
+            dp[i] = ans+1;
+        }
+        return dp[0];
+    }
     public static int fun(int n,int i, int[]nums, int[] mem){
         if(i == n-1){
             return mem[i] = 0;
@@ -23,7 +45,7 @@ class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
         int[] mem = new int[n];
-        
-        return fun(n,0,nums,mem);  
+        return tab(n,nums);
+        //return fun(n,0,nums,mem);  
     }
 }
