13a31007d0e084bdc33aff6e508c879810ee20ae
Kunal Kumar Shrivastava <31282058+Kunalkshrivastava@users.noreply.github.com>
2022-04-27 13:37:38 +0530

    Time: 2 ms (38.62%), Space: 54.4 MB (28.41%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -21,6 +21,27 @@ class Solution {
                     matrix[i][j] = 0;
             }
         }
+        /*
+        boolean col1 = false;
+        
+        for(int i=0; i< matrix.length; i++){
+                if(matrix[i][0] ==0) col1 =true;
+            for(int j= 1; j< matrix[0].length ; j++){
+                
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i= m-1; i>=0; i--){
+            for(int j= n-1; j>=1 ; j--){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+                if(col1 == true)
+                    matrix[i][0] = 0;
+            }
+        }*/
 
     }
 }
